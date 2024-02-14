4ec90a9c03df3ea521038c6ad754aad265624cbd
Cobraxs <josevillatoro2003@gmail.com>
2024-02-14 12:55:23 +0100

    Last save in Home  14/02/2023

--- EjerciciosClaseJava/src/jerarquia/Empleado.java
+++ EjerciciosClaseJava/src/jerarquia/Empleado.java
@@ -1,5 +1,54 @@
 package jerarquia;
 
 public class Empleado {
+	
+	protected String nombre;
+	
+	Empleado(String nombre){
+		this.nombre=nombre;
+	}
+	
+	public String toString() {
+		
+		return "nombre:"+this.nombre;
+	}
 
+}
+
+class Directivo  extends Empleado{
+
+	Directivo(String nombre) {
+		super(nombre);
+
+	}
+	
+}
+
+
+ class Operario  extends Empleado{
+
+	Operario(String nombre) {
+		super(nombre);
+
+	}
+	
+}
+ 
+ 
+class Oficial extends Operario{
+	
+	Oficial(String nombre) {
+		super(nombre);
+
+	}
+	
+}
+
+class Tecnico extends Operario{
+	
+	Tecnico(String nombre) {
+		super(nombre);
+
+	}
+	
 }
---
+++ EjerciciosClaseJava/src/jerarquia/main.java
@@ -0,0 +1,11 @@
+package jerarquia;
+
+public class main {
+
+	public static void main(String[] args) {
+
+		Tecnico Pedro=new Tecnico("Pedro");
+		System.out.println(Pedro.toString());
+	}
+
+}
